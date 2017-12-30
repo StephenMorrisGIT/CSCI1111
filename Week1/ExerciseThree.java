41614b93f6b34d86208a66b84c92b25b2f523f7f
Stephen Morris <smorris11@gwu.edu>
2017-12-29 20:07:41 -0500

    Week 1 Excercise 1

---
+++ Week1/ExerciseThree.java
@@ -0,0 +1,20 @@
+public class ExerciseThree
+{
+	public static void main (String[] args)
+	{
+	printStar();
+	}
+	public static void printStar()
+	{
+		System.out.println("        *");
+		System.out.println("     *     *");
+		System.out.println("   *         *");
+		System.out.println("     *");
+		System.out.println("       *");
+		System.out.println("          *");
+		System.out.println("  *         *");
+		System.out.println("   *       *");
+		System.out.println("     *   *");
+		System.out.println("       *");
+	}
+}
