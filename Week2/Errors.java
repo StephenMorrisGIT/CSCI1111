01d8e10db0d43f0df8f93ca54008fc108186c49c
Stephen Morris <smorris11@gwu.edu>
2017-12-31 16:33:35 -0500

    Week2 folder

---
+++ Week2/Errors.java
@@ -0,0 +1,52 @@
+public class Errors
+{
+	
+	public static void main(String[] args)
+	{
+
+		//This program will print SEAS
+		sLetter();
+		eLetter();
+		aLetter();
+		sLetter();
+		
+	}
+
+	public static void eLetter()
+	{
+
+		System.out.println("* * * *");
+		System.out.println("*      ");
+		System.out.println("*      ");
+		System.out.println("* * * *");
+		System.out.println("*      ");
+		System.out.println("*      ");
+		System.out.println("* * * *\n");
+	}
+	public static void aLetter()
+	{
+
+			System.out.println("* * * *");
+			System.out.println("*     *");
+			System.out.println("*     *");
+			System.out.println("* * * *");
+			System.out.println("*     *");
+			System.out.println("*     *");
+			System.out.println("*     *\n");
+
+	}
+
+  	public static void sLetter()
+  	{
+
+		System.out.println("* * * *");
+		System.out.println("*      ");
+		System.out.println("*      ");
+		System.out.println("* * * *");
+		System.out.println("      *");
+		System.out.println("      *");
+		System.out.println("* * * *\n");
+
+	}
+
+}
