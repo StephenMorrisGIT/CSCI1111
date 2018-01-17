955a6a691bd71d36b64e8c0ccc694ea62def2a69
Stephen Morris <smorris11@gwu.edu>
2018-01-17 09:54:43 -0500

    Adding ClassExercise class andJava

---
+++ Week4/ClassExcercise.java
@@ -0,0 +1,14 @@
+public class 
+{
+	public static void main(String[] args)
+	{
+	boolean a = 3<5;
+	boolean b = 9>4;
+	boolean c = (3.3==3.0);
+	int f =5;
+	int g = 7;
+	boolean d = f>=g;
+	System.out.println("a:" + a);
+	System.out.println("d:" + d);
+	}
+}
