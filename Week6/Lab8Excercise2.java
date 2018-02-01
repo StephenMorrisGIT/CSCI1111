570e438096f7a9407087b7e24acb41876483b2b1
Stephen Morris <smorris11@gwu.edu>
2018-02-01 12:08:41 -0500

    Adding Lab8Exercise2 class and java

---
+++ Week6/Lab8Excercise2.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Lab8Excercise2
+{
+	public static void main(String[] arg)
+	{
+		infinity();
+		//twoInfinityAndBeyond();
+	}
+	public static void infinity()
+	{
+	int a = 0;
+	while(a <= 100)
+		{
+		System.out.println("HI ROBBIE");
+		}
+	}
+	public static void twoInfinityAndBeyond()
+	{
+	int a = 0;
+	do
+		{
+		System.out.println("HI KATE");
+		}
+		while(a<=100);
+	}
+}
