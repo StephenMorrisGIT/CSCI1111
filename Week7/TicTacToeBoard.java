44b07df873482b1c302764254d37cef7dcc196c0
Stephen Morris <smorris11@gwu.edu>
2018-02-12 18:41:23 -0500

    Adding TicTacToeBoard class and java

---
+++ Week7/TicTacToeBoard.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class TicTacToeBoard
+{
+	public static void main(String[] args)
+	{
+		System.out.println(" X" + " | " + "X" + " | " + "X");
+		System.out.println("-----------");
+		System.out.println(" O" + " | " + "O" + " | " + "O");
+		System.out.println("-----------");
+		System.out.println(" X" + " | " + "X" + " | " + "X");
+	}
+}
