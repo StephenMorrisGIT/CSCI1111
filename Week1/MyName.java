c2af4467687fc228e9e82d8815dced7b7c210096
Stephen Morris <smorris11@gwu.edu>
2018-04-30 17:14:10 -0400

    MyName with added comments

--- Week1/MyName.java
+++ Week1/MyName.java
@@ -1,5 +1,8 @@
+// Prints the first letter of my first name using only println statements
+
 public class MyName
 {
+	// All code contained in main
 	public static void main (String[] args)
 	{
 		System.out.println("        *");
