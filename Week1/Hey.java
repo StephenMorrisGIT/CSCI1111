7d1f5c3ec24f1e70b80efd42129588c5cd346925
Stephen Morris <smorris11@gwu.edu>
2018-05-02 21:38:21 -0400

    Comments added to Hey.java

--- Week1/Hey.java
+++ Week1/Hey.java
@@ -1,7 +1,10 @@
+// Public class that has one method (main) and prints one statement
 public class Hey
 {
+	// Only method
 	public static void main(String[] args)
 	{
-		System.out.println("Hey!!!!!");
-	}	
+		// Only statement, prints Hey!!!
+		System.out.println("Hey!!!");
+	}
 }
