63bd345fd9a4c81cdf85585832092962be8ee8c7
Stephen Morris <smorris11@gwu.edu>
2018-05-03 22:20:43 -0400

    Adding comments to ExerciseThree

--- Week1/ExerciseThree.java
+++ Week1/ExerciseThree.java
@@ -1,9 +1,12 @@
+// Public class with two methods, implementing MyName
 public class ExerciseThree
 {
+	// Only calls printStar
 	public static void main (String[] args)
 	{
-	printStar();
+		printStar();
 	}
+	// Implements all the println statements from main in MyName
 	public static void printStar()
 	{
 		System.out.println("        *");
