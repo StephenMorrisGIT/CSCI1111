f46b8b912581cb349850ae69d8d92d2d6520e041
Stephen Morris <smorris11@gwu.edu>
2018-05-04 21:11:35 -0400

    Bing class and java with added comments

--- Week1/Bing.java
+++ Week1/Bing.java
@@ -1,12 +1,24 @@
+/*
+** This program implements java music and creates and plays a single note.
+** The program only has one method (main) and only one statement in main.
+**
+** Created for CSCI1111
+** Designer: Stephen Morris
+** Date Created:						Last Modified: May 4th, 2018
+** Email: smorris.winchester@gmail.com
+*/
+
 import jm.JMC;
 import jm.music.data.*;
 import jm.util.Play;
 
-public final class Bing implements JMC 
+// Final declaration so JMC implementation cannot be changed
+public final class Bing implements JMC
 {
-
-	public static void main(String[] args) 
+	// Only method in Bing class
+	public static void main(String[] args)
 	{
+		// Note is created and played in the same statement
 		Play.midi(new Note(45, QN));	//C4 can be any number between 0-128
 	}
 }
