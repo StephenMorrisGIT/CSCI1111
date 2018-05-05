939ffa2dad7265e7d225dadb07c83e0aa51af87a
Stephen Morris <smorris11@gwu.edu>
2018-05-05 18:35:12 -0400

    Comments added to Sound

--- Week2/Sound.java
+++ Week2/Sound.java
@@ -1,11 +1,24 @@
+/*
+** This program uses java music and creates and plays a single note.
+** The program only has one method (main) and uses two statements in main.
+**
+** Created for CSCI1111
+** Designer: Stephen Morris
+** Date Created:						Last Modified: May 5th, 2018
+** Email: smorris.winchester@gmail.com
+*/
+
 import jm.util.*;
 import jm.music.data.*;
 
 public class Sound
 {
+	// Only method
 	public static void main (String[] args)
 	{
-		Note n = new Note(60, 8.0 , 127 , 0);
+		// Creates note n(pitch, duration, pan, loudness)
+		Note n = new Note(60 , 8.0 , 127 , 0);
+		// Plays notes
 		Play.midi(n);
 	}
 }
